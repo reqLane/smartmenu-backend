0dae912699a6bd63d113ae51ab0f836bc177f9ee
reqLane <vlad5000191@gmail.com>
2024-11-20 18:35:10 +0200

    Order cooked status logic

--- src/main/java/com/naukma/smartmenubackend/order_item/OrderItemService.java
+++ src/main/java/com/naukma/smartmenubackend/order_item/OrderItemService.java
@@ -1,6 +1,9 @@
 package com.naukma.smartmenubackend.order_item;
 
 import com.naukma.smartmenubackend.exception.InvalidOrderItemDataException;
+import com.naukma.smartmenubackend.order.OrderRepo;
+import com.naukma.smartmenubackend.order.model.Order;
+import com.naukma.smartmenubackend.order.status.OrderStatus;
 import com.naukma.smartmenubackend.order_item.model.OrderItem;
 import com.naukma.smartmenubackend.order_item.model.OrderItemDTO;
 import com.naukma.smartmenubackend.utils.DTOMapper;
@@ -15,9 +18,11 @@ import java.util.Set;
 @Service
 public class OrderItemService {
     private final OrderItemRepo orderItemRepo;
+    private final OrderRepo orderRepo;
 
-    public OrderItemService(OrderItemRepo orderItemRepo) {
+    public OrderItemService(OrderItemRepo orderItemRepo, OrderRepo orderRepo) {
         this.orderItemRepo = orderItemRepo;
+        this.orderRepo = orderRepo;
     }
 
     // BUSINESS LOGIC
@@ -31,6 +36,15 @@ public class OrderItemService {
         orderItem.setIsDone(true);
 
         orderItem = save(orderItem);
+
+        Order order = orderItem.getOrder();
+        boolean isCooked = order.getOrderItems()
+                .stream()
+                .allMatch(OrderItem::getIsDone);
+        if (isCooked)
+            order.setStatus(OrderStatus.COOKED);
+        orderRepo.save(order);
+
         return DTOMapper.toDTO(orderItem);
     }
 
